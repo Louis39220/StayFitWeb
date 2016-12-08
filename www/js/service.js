e14e002271a4470a774c3e9e7cecf57317280930
MaximeCollot <maxime.collot.69@gmail.com>
2016-12-08 11:37:32 +0100

    Changement de repo

---
+++ www/js/service.js
@@ -0,0 +1,27 @@
+angular.module('services', [])
+
+.factory('userService', function($http) {
+	var user = [];
+
+	return {
+		authenticate: function(user){
+			var data = {email: user.mail, psw: user.password};
+			console.log(data);
+			return $http({
+				method: 'POST',
+				url: 'http://localhost:8080/userConnect/login',
+				params: data
+			})
+		},
+
+		suscribe: function(user){
+			var data = {email: user.mail, psw: user.password};
+			console.log(data);
+			return $http({
+				method: 'POST',
+				url: 'http://localhost:8080/userConnect/create',
+				params: data
+			})
+		}
+	}
+})
