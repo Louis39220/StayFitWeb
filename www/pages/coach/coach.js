143ea94ba7941b1ef7390dd72181854508533be6
Florian Manier <Florian Manier>
2017-01-05 15:32:57 +0100

    debut coach

--- www/pages/coach/coach.js
+++ www/pages/coach/coach.js
@@ -3,4 +3,33 @@ angular.module('coach.controllers', [])
 .controller('CoachCtrl', function($scope) {
 
 
+
+
+
+  $scope.choices = [{id: 'choice1'}];
+
+  
+
+  $scope.addNewChoice = function() {
+
+    var newItemNo = $scope.choices.length+1;
+
+    $scope.choices.push({'id':'choice'+newItemNo});
+
+  };
+
+    
+
+  $scope.removeChoice = function() {
+
+    var lastItem = $scope.choices.length-1;
+
+    $scope.choices.splice(lastItem);
+
+  };
+
+  
+
+  
+
 });
