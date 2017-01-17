17cbdc9035bb61916740ab94fb07c0c1d3a6eb02
RudyDeal <rudy.deal.rd@gmail.com>
2017-01-17 14:16:40 +0100

    fix redirection

--- www/js/app.js
+++ www/js/app.js
@@ -225,5 +225,5 @@ angular.module('starter', ['ionic', 'menu.controllers','objectif.controllers', '
     })
 
   // if none of the above states are matched, use this as the fallback
-  //$urlRouterProvider.otherwise('/app/welcome');
+  $urlRouterProvider.otherwise('/app/');
 });
